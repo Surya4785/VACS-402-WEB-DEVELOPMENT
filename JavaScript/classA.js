6561ed4862008db5f28867ed3820019427b37e7e
Surya4785 <suryaprakash8707yadav@gmail.com>
2025-04-16 12:22:11 +0530

    claas  maps

---
+++ JavaScript/classA.js
@@ -0,0 +1,13 @@
+// classes in javaScript
+
+class classA {
+    constructor(name, age){
+        this.name = name;
+        this.age = age;
+    }
+    display(){
+        console.log(`Person name is ${this.name} and age is ${this.age}`);
+    }
+}
+let myInfo = new classA("Ajay", 23);
+myInfo.display();  
---
+++ JavaScript/maps.js
@@ -0,0 +1,19 @@
+// // javaScript Maps
+
+const maps = new Map([
+    ["name", "Surya"],
+    ["age", "21"],
+    ["mobile", "870xxxx557"],
+    ["email", "surya123@gmail.com"],
+    ["isMarried",false]
+]);
+
+console.log(maps);
+console.log(maps.get("name"));
+
+// methods of Maps
+
+// console.log(maps.set("name", "Mohit"));
+// console.log(maps.delete("age"));
+// console.log(maps.has("age"));
+// console.log(maps.clear)
