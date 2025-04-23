03125c876794c5ddced211a9bd14145f317607bf
Surya4785 <suryaprakash8707yadav@gmail.com>
2025-04-23 12:07:45 +0530

    modified

--- JavaScript/class.js
+++ JavaScript/class.js
@@ -1,4 +1,17 @@
-// write a program to display age, city and course using the class.
+// To display the name using callback function.
+function displayName(name) {
+    console.log("Name: " + name);
+  }
+  
+  function getName(callback) {
+    const name = "Surya"; 
+    callback(name); 
+  }
+  
+  getName(displayName);
+  
+
+// To display name, age, city and course using the class.
 class Student {
     constructor(name, age, city, course) {
         this.name = name;
@@ -14,6 +27,5 @@ class Student {
     }
   }
   
-  const student1 = new Student("Surya",21, "Ghaziabad", "Computer Science");
+  const student1 = new Student("Surya",21, "Ghaziabad", "Computer Science (IT)");
   student1.displayDetails();
-  
